470af347af14d011937e90358a111b4a3f23450e
Rod Johnson <johnsonroda@gmail.com>
2018-03-05 11:54:50 +0000

    Initial commit from Atomist

---
+++ src/main/java/atomist/Mark1Application.java
@@ -0,0 +1,12 @@
+package atomist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mark1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mark1Application.class, args);
+	}
+}
